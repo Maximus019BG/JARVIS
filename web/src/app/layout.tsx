1a51d200d205e8edcf412a882c08217e59dffadf
Maximus019BG <maxralev019@gmail.com>
2025-10-16 22:17:35 +0300

    build: default t3 app init

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import "~/styles/globals.css";
+
+import { type Metadata } from "next";
+import { Geist } from "next/font/google";
+
+export const metadata: Metadata = {
+  title: "JARVIS Web",
+  description: "Web Application to customize your Job Acceleration Reference Visual Interface System",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+const geist = Geist({
+  subsets: ["latin"],
+  variable: "--font-geist-sans",
+});
+
+//TODO: use better auth and add language support
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${geist.variable}`}>
+      <body>{children}</body>
+    </html>
+  );
+}
