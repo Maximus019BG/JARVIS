2948db4fe7ae8f2649c9cc73585b7d14b8f64708
Maximus019BG <maxralev019@gmail.com>
2025-11-28 21:02:30 +0200

    feat: github login plus favicom

--- web/src/env.js
+++ web/src/env.js
@@ -33,8 +33,8 @@ export const env = createEnv({
         return parsed;
       })
       .default("1440"), // in minutes (1 day)
-    // GITHUB_CLIENT_ID: z.string().min(1),
-    // GITHUB_CLIENT_SECRET: z.string().min(1),
+    GITHUB_CLIENT_ID: z.string().min(1),
+    GITHUB_CLIENT_SECRET: z.string().min(1),
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
@@ -60,8 +60,8 @@ export const env = createEnv({
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
     BETTER_AUTH_SECRET: process.env.BETTER_AUTH_SECRET,
-    // GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
-    // GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     BETTER_AUTH_ORGANIZATION_LIMIT: process.env.BETTER_AUTH_ORGANIZATION_LIMIT,
