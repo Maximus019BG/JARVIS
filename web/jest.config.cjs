c7c88e7e8cb18dc398269a83db944a3bac7880f2
RalevEnlapse <mralev@enlapse.com>
2026-02-11 10:38:40 +0200

    tests: new tests and ux map

--- web/jest.config.cjs
+++ web/jest.config.cjs
@@ -4,11 +4,16 @@ module.exports = {
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.test.ts'],
   transform: {
-    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }]
+    '^.+\\.ts$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.json' }],
+    // Use babel-jest for ESM deps inside node_modules (e.g. nanoid).
+    '^.+\\.m?[jt]sx?$': 'babel-jest'
   },
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1'
   },
+  // pnpm nests deps under node_modules/.pnpm/..., so match that structure too.
+  // Allow nanoid to be transformed instead of failing on `import`.
+  transformIgnorePatterns: ['node_modules/(?!\\.pnpm/|nanoid/)'],
   clearMocks: true,
   restoreMocks: true
 };
